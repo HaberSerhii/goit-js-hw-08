82defc49c28fd77ed3f5ab12c60970e475824a02
Serhii Haber <haberserhii@icloud.com>
2023-08-14 11:59:29 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,2 +1,23 @@
+import LightBoxSimple from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', createMarkup);
+
+const createMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    const item = `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`;
+
+    return item;
+  })
+  .join('');
+
+new LightBoxSimple('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
